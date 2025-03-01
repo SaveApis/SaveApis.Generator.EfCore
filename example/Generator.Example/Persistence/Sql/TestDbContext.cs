3ad5ba9c0e780f13dba686ff341fa5c7c2de94fd
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-01 17:49:25 +0100

    feat: DbContextFactory generator
    Author: Felix Hake

---
+++ example/Generator.Example/Persistence/Sql/TestDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaveApis.Common.Infrastructure.Persistence.Sql;
+
+namespace Generator.Example.Persistence.Sql;
+
+public class TestDbContext(DbContextOptions options) : BaseDbContext(options)
+{
+    protected override void RegisterEntities(ModelBuilder modelBuilder)
+    {
+    }
+
+    protected override string Schema => "Test";
+}
---
+++ src/SaveApis.Generator.EfCore/DbContextFactoryGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SaveApis.Generator.EfCore;
+
+[Generator]
+public class DbContextFactoryGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var provider = context.SyntaxProvider.CreateSyntaxProvider((node, _) => Filter(node), (syntaxContext, _) => Transform(syntaxContext));
+
+        context.RegisterSourceOutput(provider, GenerateFactoryInterface);
+        context.RegisterSourceOutput(provider, GenerateFactory);
+    }
+
+    private static bool Filter(SyntaxNode node)
+    {
+        if (node is not ClassDeclarationSyntax classDeclarationSyntax)
+        {
+            return false;
+        }
+
+        if (!classDeclarationSyntax.Identifier.ToString().EndsWith("DbContext"))
+        {
+            return false;
+        }
+
+        return classDeclarationSyntax.BaseList?.Types.Any(x => x.Type.ToString() == "BaseDbContext") == true;
+    }
+
+    private static ClassDeclarationSyntax Transform(GeneratorSyntaxContext context)
+    {
+        return (ClassDeclarationSyntax)context.Node;
+    }
+
+    private static void GenerateFactoryInterface(SourceProductionContext context, ClassDeclarationSyntax syntax)
+    {
+        var namespaceName = syntax.FirstAncestorOrSelf<FileScopedNamespaceDeclarationSyntax>()?.Name.ToString() ??
+                            throw new InvalidOperationException("Namespace not found");
+
+        var className = syntax.Identifier.ToString();
+
+        var source = $$"""
+                      using {{namespaceName}};
+                      
+                      namespace {{namespaceName}}.Factories;
+
+                      public interface I{{className}}Factory
+                      {
+                          {{className}} Create();
+                      }
+                      """;
+
+        context.AddSource($"I{className}Factory.g.cs", source);
+    }
+
+    private static void GenerateFactory(SourceProductionContext context, ClassDeclarationSyntax syntax)
+    {
+        var namespaceName = syntax.FirstAncestorOrSelf<FileScopedNamespaceDeclarationSyntax>()?.Name.ToString() ??
+                            throw new InvalidOperationException("Namespace not found");
+        var className = syntax.Identifier.ToString();
+
+        var source = $$"""
+                       using {{namespaceName}};
+                       using Microsoft.Extensions.Configuration;
+                       using SaveApis.Common.Infrastructure.Persistence.Sql.Factories;
+
+                       namespace {{namespaceName}}.Factories;
+
+                       public class {{className}}Factory(IConfiguration configuration) : BaseDbContextFactory<{{className}}>(configuration), I{{className}}Factory
+                       {
+                           public {{className}}Factory() : this(new ConfigurationBuilder().AddInMemoryCollection().Build())
+                           {
+                           }
+                           
+                           public {{className}} Create()
+                           {
+                                return CreateDbContext([]);
+                           }
+                       }
+                       """;
+
+        context.AddSource($"{className}Factory.cs", source);
+    }
+}
