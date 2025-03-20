2e15b68d755efc8f3d07c98a58bb2081b062bd31
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-20 20:44:15 +0100

    fix(entity): Fix properties with keyword name
    Author: Felix Hake

--- example/Generator.Example/Domains/Entities/TestEntity.cs
+++ example/Generator.Example/Domains/Entities/TestEntity.cs
@@ -7,4 +7,6 @@ public partial class TestEntity : IEntity
     public Guid Id { get; }
     public string Test { get; }
     public int TestInt { get; private set; }
+    public string Class { get; }
+    public string Namespace { get; private set; }
 }
