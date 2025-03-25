62ebd837c84796873884d3f6a669cf7f61837371
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-25 17:46:33 +0100

    fix(factory): Fix wrong base factory Namespace
    Author: Felix Hake

--- src/SaveApis.Generator.EfCore/DbContextFactoryGenerator.cs
+++ src/SaveApis.Generator.EfCore/DbContextFactoryGenerator.cs
@@ -69,7 +69,7 @@ public class DbContextFactoryGenerator : IIncrementalGenerator
         builder.AppendLine($"using {namespaceName};");
         builder.AppendLine("using MediatR;");
         builder.AppendLine("using Microsoft.Extensions.Configuration;");
-        builder.AppendLine("using SaveApis.Common.Infrastructure.Persistence.Sql.Factories;");
+        builder.AppendLine("using SaveApis.Common.Domains.EfCore.Infrastructure.Persistence.Sql;");
         builder.AppendLine();
         builder.AppendLine($"namespace {namespaceName}.Factories;");
         builder.AppendLine();
